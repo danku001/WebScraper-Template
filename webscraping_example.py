2ac9f3d1b79668069030b2ddccf473d629a01348
danku001 <65789554+danku001@users.noreply.github.com>
2022-04-02 12:14:33 -0400

    Update webscraping_example.py
    Added getpass module and HTTPBasicAuth authentication to deal with username and password authentication.

--- webscraping_example.py
+++ webscraping_example.py
@@ -1,9 +1,18 @@
 """
 Webscraping example
+made modifications to include getpass4 and getpass
+
+getpass -> builtin python package
+getpass -> updated version...use this when you have real password and stuff.
 """
 
 import requests
 from bs4 import BeautifulSoup as bs
+#import getpass4 as gp #prompts user for password without echoing to screen
+import getpass as gp
+from requests.auth import HTTPBasicAuth    #Basic authentication credentials
+                                           #If different kind of authentication then change import
+
 
 #getting or loading the webpage content
 link = "https://keithgalli.github.io/web-scraping/example.html"
@@ -11,7 +20,10 @@ link1 = "http://www.google.com/nothere"
 
 #requests implements browser style ssl verification
 try:
-    r = requests.get(link)
+    r = requests.get(
+        link,
+        auth = HTTPBasicAuth( input('username: '), gp.getpass('Pword: ') )
+        )
     r.raise_for_status()
 except requests.exceptions.HTTPError as err404:
     #will occur if the webpage doesn't exist
